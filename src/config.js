0d56ce0b10ec0602e97981e536aaca59c75cf4ea
Aapeli Haanpuu <aapeli.haanpuu@hoxhunt.com>
2024-05-10 13:52:27 +0300

    Configure via env variables

--- src/config.js
+++ src/config.js
@@ -5,10 +5,26 @@ const getLocalConfig = () => {
   try {
     return require("./config.local.js");
   } catch (e) {
+    const email = process.env.USER_EMAIL;
+    if (!email) {
+      throw new Error(
+        "Please provide a local config in 'src/config.local.js' or the email of the user you want to authenticate as in the USER_EMAIL environment variable"
+      );
+    }
     console.log(
-      'Local config not found. You should add one to "config.local.js"'
+      'Local config not found. Using default values'
     );
-    return {};
+    const fullNameFromEmail = email.split("@")[0].split(".").map((s) => s.charAt(0).toUpperCase() + s.slice(1)).join(" ");
+    const [firstName, lastName] = fullNameFromEmail.split(" ");
+    return {
+      user: {
+        userName: email,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        displayName: fullNameFromEmail,
+      }
+    };
   }
 };
 
--- src/startServer.js
+++ src/startServer.js
@@ -1,8 +1,14 @@
 const { runServer } = require("saml-idp");
 const config = require("./config");
 
+const audience = process.env.AUDIENCE_URL;
+
+if (!audience) {
+  throw new Error("Please provide an AUDIENCE_URL environment variable");
+}
+
 runServer({
   acsUrl: `https://foo.okta.com/auth/saml20/assertion-consumer`,
-  audience: `https://foo.okta.com/auth/saml20/metadata`,
-  ...config,
+  audience,
+  config,
 });
